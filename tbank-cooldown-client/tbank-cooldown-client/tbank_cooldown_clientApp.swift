a4aac23d6c74621bdb9cc9af6ab2581f87b7722d
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-12-05 17:59:58 +0300

    init client

---
+++ tbank-cooldown-client/tbank-cooldown-client/tbank_cooldown_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  tbank_cooldown_clientApp.swift
+//  tbank-cooldown-client
+//
+//  Created by Александр Павлицкий on 05.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct tbank_cooldown_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tbank-cooldown-client/tbank-cooldown-clientUITests/tbank_cooldown_clientUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  tbank_cooldown_clientUITestsLaunchTests.swift
+//  tbank-cooldown-clientUITests
+//
+//  Created by Александр Павлицкий on 05.12.2025.
+//
+
+import XCTest
+
+final class tbank_cooldown_clientUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
