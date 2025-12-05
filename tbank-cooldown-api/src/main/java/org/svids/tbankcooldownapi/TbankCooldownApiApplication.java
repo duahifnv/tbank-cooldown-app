8558294b7dfb5b706b165a61484763eaa6f25cd3
Максим Фоминцев <max135603@gmail.com>
2025-12-05 17:31:49 +0300

    spring boot application initial

---
+++ tbank-cooldown-api/src/main/java/org/svids/tbankcooldownapi/TbankCooldownApiApplication.java
@@ -0,0 +1,13 @@
+package org.svids.tbankcooldownapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbankCooldownApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbankCooldownApiApplication.class, args);
+    }
+
+}
---
+++ tbank-cooldown-api/src/test/java/org/svids/tbankcooldownapi/TbankCooldownApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.svids.tbankcooldownapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbankCooldownApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
